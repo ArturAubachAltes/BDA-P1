6e856eaaef33423f96f02fbec88801974d9af80f
Paula Galvez <paula.galvez.serrano@estudiantat.upc.edu>
2024-03-19 19:37:54 +0100

    formatting

---
+++ data_collectors.py
@@ -0,0 +1,17 @@
+class DataCollector:
+    def __init__(self):
+        self.dataset = ...
+        self.carpeta = ...
+    
+
+    #Métode que llegeix els arxius de la carpeta i agafa la data més recent
+        
+    #Métode que comprova si la data més recent és de fa més d'un mes
+        
+    #Métode que força el descarregat sense tenir en compte les dates
+   
+    #Métode que descarrega l'arxiu si fa més d'un mes que no s'ha actualitzat
+        
+    #Métode que elimina els arxius antics i deixa únicament el de la data més recent
+        
+    #Métode que fa un merge de l'arxiu nou i el vell
