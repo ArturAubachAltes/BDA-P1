16f044f3f5565ff851f0dfda1a39eb2307f05b16
Paula Galvez <paula.galvez.serrano@estudiantat.upc.edu>
2024-04-11 13:52:04 +0200

    dataQuality

--- DataCollectors.py
+++ DataCollectors.py
@@ -63,4 +63,4 @@ def datacollectors(income= False, datasearch= False, sales= False):
 
 
 
-datacollectors(income= True, datasearch= True, sales= True)
+datacollectors(income= False, datasearch= False, sales= False)
