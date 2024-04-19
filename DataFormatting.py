d355d7ccd6a016f3757821a769ae6bedecd4e5ba
AubachAltesArtur <artur.aubach@gmail.com>
2024-04-19 10:38:30 +0200

    dataformating

--- DataFormatting.py
+++ DataFormatting.py
@@ -10,41 +10,69 @@ import os
 if not 'spark' in globals():
   spark = SparkSession.builder.getOrCreate()
 
-# Directori on es troben els fitxers CSV
-directory_path_income = "datalake/income_data/"
-directory_path_sales = "datalake/sales_data/"
-directory_path_shops = "datalake/shops_data/"
+def descargar_spark(link:str):
 
-# Llista tots els fitxers en el directori especificat i selecciona el primer CSV
-files_income = os.listdir(directory_path_income)
-parquet_files = [file for file in files_income if file.endswith('.parquet')]
-first_parquet_file_income = parquet_files[0] if parquet_files else None
-
-files_sales = os.listdir(directory_path_sales)
-parquet_files = [file for file in files_sales if file.endswith('.parquet')]
-first_parquet_file_sales = parquet_files[0] if parquet_files else None
-
-files_shops = os.listdir(directory_path_shops)
-parquet_files = [file for file in files_shops if file.endswith('.parquet')]
-first_parquet_file_shops = parquet_files[0] if parquet_files else None
+  files = os.listdir(link)
+  parquet_files = [file for file in files if file.endswith('.parquet')]
+  first_parquet_file_shops = parquet_files[0] if parquet_files else None
 
 
-# Camí complet al primer fitxer CSV
-income_path = os.path.join(directory_path_income, first_parquet_file_income)
-sales_path = os.path.join(directory_path_sales, first_parquet_file_sales)
-shop_path = os.path.join(directory_path_shops, first_parquet_file_shops)
+  # Camí complet al primer fitxer CSV
+  path = os.path.join(link, first_parquet_file_shops)
 
+  # Carreguem les dades, especificant que la primera fila és la capçalera
+  income = spark.read.option("header", "true").parquet(path)
 
+  # Mostrar les dades
+  income.show()
+  return income
 
-# Carreguem les dades, especificant que la primera fila és la capçalera
-income = spark.read.option("header", "true").parquet(income_path)
-sales = spark.read.option("header", "true").parquet(sales_path)
-shop = spark.read.option("header", "true").parquet(shop_path)
+def formating(income: bool, sales: bool, shops: bool):
+    if income == True:
+        descargar_spark("datalake/income_data/")
+        print(f"Database income actualitzada")
 
+    elif shops == True:
+        descargar_spark("datalake/sales_data/")
+        print(f"Database shops actualitzada")
+
+    elif sales == True:
+        descargar_spark("datalake/shops_data/")
+        print(f"Database sales actualitzada")
 
 
 
-# Mostrar les dades
-income.show()
-sales.show()
-shop.show()
+
+# # Llista tots els fitxers en el directori especificat i selecciona el primer CSV
+# files_income = os.listdir(directory_path_income)
+# parquet_files = [file for file in files_income if file.endswith('.parquet')]
+# first_parquet_file_income = parquet_files[0] if parquet_files else None
+
+# files_sales = os.listdir(directory_path_sales)
+# parquet_files = [file for file in files_sales if file.endswith('.parquet')]
+# first_parquet_file_sales = parquet_files[0] if parquet_files else None
+
+# files_shops = os.listdir(directory_path_shops)
+# parquet_files = [file for file in files_shops if file.endswith('.parquet')]
+# first_parquet_file_shops = parquet_files[0] if parquet_files else None
+
+
+# # Camí complet al primer fitxer CSV
+# income_path = os.path.join(directory_path_income, first_parquet_file_income)
+# sales_path = os.path.join(directory_path_sales, first_parquet_file_sales)
+# shop_path = os.path.join(directory_path_shops, first_parquet_file_shops)
+
+
+
+# # Carreguem les dades, especificant que la primera fila és la capçalera
+# income = spark.read.option("header", "true").parquet(income_path)
+# sales = spark.read.option("header", "true").parquet(sales_path)
+# shop = spark.read.option("header", "true").parquet(shop_path)
+
+
+
+
+# # Mostrar les dades
+# income.show()
+# sales.show()
+# shop.show()
