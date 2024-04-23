2d04f546127aa61fdba08449b69e6a2e6d7adf8d
Paula Galvez <paula.galvez.serrano@estudiantat.upc.edu>
2024-04-23 22:53:53 +0200

    a

--- DataCollectors.py
+++ DataCollectors.py
@@ -61,4 +61,4 @@ def datacollectors(income= False, datasearch= False, sales= False):
                 print(f"Database {nom} no actualitzada")
 
 
-datacollectors(income=True, datasearch=False, sales=True)
+datacollectors(income=False, datasearch=False, sales=False)
--- DataFormatting.py
+++ DataFormatting.py
@@ -1,84 +1,45 @@
-'''
-Data Formatting Pipline: pasar de LandingZone a FormattedZone
-!pip install pyspark
-'''
 from pyspark.sql import SparkSession
 import os
-
-
-# Inicialitzem SparkSession
-if not 'spark' in globals():
-  spark = SparkSession.builder.getOrCreate()
-
-
-
-
-def formating(income: bool = False, sales: bool = False, shops: bool = False):
-    if income == True:
-        descargar_spark("datalake/income_data/")
-        print(f"Database income actualitzada")
-
-    elif shops == True:
-        descargar_spark("datalake/sales_data/")
-        print(f"Database shops actualitzada")
-
-    elif sales == True:
-        descargar_spark("datalake/shops_data/")
-        print(f"Database sales actualitzada")
-
-
-
-import os
-from pyspark.sql import SparkSession
 import duckdb
+import pandas as pd
 
-spark = SparkSession.builder.master("local[*]").appName("ParquetToDuckDB").getOrCreate()
 
+def DataFormatting(quines_act):
 
-for source in ["flights","airbnb","weather"]:
-    df = spark.read.parquet(f"{source}_data.parquet")
-    pandas_df = df.toPandas()
+    # Inicialización de Spark
+    spark = SparkSession.builder \
+        .config("spark.jars", "duckdb.jar") \
+        .appName("FormattedZone") \
+        .getOrCreate()
 
+    # Conexión a la base de datos DuckDB
     con = duckdb.connect(database='/database.duckdb')
 
-    con.register(f"{source}_view", pandas_df)
+    for data in quines_act:
+        directory_path = os.path.join("datalake", data)
+        parquet_files = [f for f in os.listdir(directory_path) if f.endswith('.parquet')]
 
-    con.execute(f"CREATE TABLE IF NOT EXISTS {source} AS SELECT * FROM {source}_view")
+        parquet_path = os.path.join(directory_path, parquet_files[0])
+        df = spark.read.parquet(parquet_path)
+        
+        # Conversión a DataFrame de Pandas
+        pandas_df = df.toPandas()
 
-    result = con.execute(f"SELECT * FROM {source} LIMIT 10").fetchall()
-    print(result)
+        # Registro del DataFrame como una vista en DuckDB
+        con.register(f"{data}_view", pandas_df)
 
+        # Creación de la tabla en la base de datos si no existe
+        con.execute(f"CREATE TABLE IF NOT EXISTS {data} AS SELECT * FROM {data}_view")
 
-# # Llista tots els fitxers en el directori especificat i selecciona el primer CSV
-# files_income = os.listdir(directory_path_income)
-# parquet_files = [file for file in files_income if file.endswith('.parquet')]
-# first_parquet_file_income = parquet_files[0] if parquet_files else None
+        # Alternativamente, si la tabla ya podría existir y quieres añadir nuevos datos:
+        con.execute(f"INSERT INTO {data} SELECT * FROM {data}_view")
+        
 
-# files_sales = os.listdir(directory_path_sales)
-# parquet_files = [file for file in files_sales if file.endswith('.parquet')]
-# first_parquet_file_sales = parquet_files[0] if parquet_files else None
-
-# files_shops = os.listdir(directory_path_shops)
-# parquet_files = [file for file in files_shops if file.endswith('.parquet')]
-# first_parquet_file_shops = parquet_files[0] if parquet_files else None
-
-
-# # Camí complet al primer fitxer CSV
-# income_path = os.path.join(directory_path_income, first_parquet_file_income)
-# sales_path = os.path.join(directory_path_sales, first_parquet_file_sales)
-# shop_path = os.path.join(directory_path_shops, first_parquet_file_shops)
+    # Cierra la conexión a la base de datos al finalizar
+    con.commit()
+    con.close()
 
 
+quines_act = ["income", "shops"]
 
-# # Carreguem les dades, especificant que la primera fila és la capçalera
-# income = spark.read.option("header", "true").parquet(income_path)
-# sales = spark.read.option("header", "true").parquet(sales_path)
-# shop = spark.read.option("header", "true").parquet(shop_path)
-
-
-
-
-# # Mostrar les dades
-# income.show()
-# sales.show()
-# shop.show()
+DataFormatting(quines_act)
