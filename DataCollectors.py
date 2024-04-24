c881aa48ae70bd9bf2783f0c913bfc602f47c870
ArturAubachAltes <artur.aubach@estudiantat.upc.edu>
2024-04-24 21:09:34 +0200

    sadasddfdsf

--- DataCollectors.py
+++ DataCollectors.py
@@ -61,4 +61,4 @@ def datacollectors(income= False, datasearch= False, sales= False):
                 print(f"Database {nom} no actualitzada")
 
 
-datacollectors(income=False, datasearch=False, sales=False)
+datacollectors(income=True, datasearch=True, sales=True)
--- DataQuality.py
+++ DataQuality.py
@@ -1,16 +1,19 @@
 # quality pipeline de les tres taules en duckdb
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
+import os
+import duckdb
+import pandas as pd
 
 
-
-def DataQuality():
+def DataQuality(income:bool= False, datasearch:bool= False, sales:bool= False, grafiques:bool= False):
     #obrim sessió spark i duckdb
     spark = SparkSession.builder \
             .config("spark.jars", "./duckdb.jar") \
             .appName("FormattedZone") \
             .getOrCreate()
     
+
     ###############
     ## AUXILIARS ##
     ###############
@@ -25,129 +28,135 @@ def DataQuality():
     ############
     ## INCOME ##
     ############
-    #carregar taula de duckdb
-    income_df = spark.read \
-        .format("jdbc") \
-        .option("url", "jdbc:duckdb:formatted_zone.duckdb") \
-        .option("dbtable", "income_data") \
-        .option("driver", "org.duckdb.DuckDBDriver") \
-        .load()
-    
-    # aplicar nateja noms columna
-    cleaned_income = income_df.select([col(c).alias(clean_column_name(c)) for c in income_df.columns])
+    if income:
+        #carregar taula de duckdb
+        income_df = spark.read \
+            .format("jdbc") \
+            .option("url", "jdbc:duckdb:formatted_zone.duckdb") \
+            .option("dbtable", "income_data") \
+            .option("driver", "org.duckdb.DuckDBDriver") \
+            .load()
 
-    # guardem taula
-    cleaned_income.write \
-        .format("jdbc") \
-        .option("url", "jdbc:duckdb:trusted_zone.duckdb") \
-        .option("dbtable", "cleaned_income") \
-        .option("driver", "org.duckdb.DuckDBDriver") \
-        .mode("overwrite") \
-        .save()
-    
-    cleaned_income.show()
+        # aplicar nateja noms columna
+        cleaned_income = income_df.select([col(c).alias(clean_column_name(c)) for c in income_df.columns])
+
+        # guardem taula
+        cleaned_income.write \
+            .format("jdbc") \
+            .option("url", "jdbc:duckdb:trusted_zone.duckdb") \
+            .option("dbtable", "cleaned_income") \
+            .option("driver", "org.duckdb.DuckDBDriver") \
+            .mode("overwrite") \
+            .save()
+        
+        cleaned_income.show()
+        print("Trusted zone de INCOME, actualitzat")
     
 
     ###########
     ## SALES ##
     ###########
-    #carregar taula de duckdb
-    sales_df = spark.read \
-        .format("jdbc") \
-        .option("url", "jdbc:duckdb:formatted_zone.duckdb") \
-        .option("dbtable", "sales_data") \
-        .option("driver", "org.duckdb.DuckDBDriver") \
-        .load()
+    if sales == True:
+        #carregar taula de duckdb
+        sales_df = spark.read \
+            .format("jdbc") \
+            .option("url", "jdbc:duckdb:formatted_zone.duckdb") \
+            .option("dbtable", "sales_data") \
+            .option("driver", "org.duckdb.DuckDBDriver") \
+            .load()
 
-    # aplicar nateja noms columna
-    cleaned_sales = sales_df.select([col(c).alias(clean_column_name(c)) for c in sales_df.columns])
+        # aplicar nateja noms columna
+        cleaned_sales = sales_df.select([col(c).alias(clean_column_name(c)) for c in sales_df.columns])
 
-    # preprocessing
-    sales_usa = cleaned_sales.filter(col("Country_/_Region") == "United States of America") # filtrar EEUU
-    sales_usa = sales_usa.dropDuplicates(subset=[col for col in sales_usa.columns if col != "row"]) # eliminar row
-    sales_usa = sales_usa.drop("Customer_Name") # eliminar customer_name
+        # preprocessing
+        sales_usa = cleaned_sales.filter(col("Country_/_Region") == "United States of America") # filtrar EEUU
+        sales_usa = sales_usa.dropDuplicates(subset=[col for col in sales_usa.columns if col != "row"]) # eliminar row
+        sales_usa = sales_usa.drop("Customer_Name") # eliminar customer_name
 
-    # no fa falta fer imputació de missings perque quan filtrem per USA no ens queden columnes amb missings
-    sales_usa = sales_usa.dropna(subset=["Postal_Code"]) # eliminar missings a postal_code
-    sales_usa = sales_usa.dropna(subset=["SubRegion"]) # eliminar missings a subregions
-    sales_usa = sales_usa.dropna(how="all") # eliminar files que missing a totes les columnes
+        # no fa falta fer imputació de missings perque quan filtrem per USA no ens queden columnes amb missings
+        sales_usa = sales_usa.dropna(subset=["Postal_Code"]) # eliminar missings a postal_code
+        sales_usa = sales_usa.dropna(subset=["SubRegion"]) # eliminar missings a subregions
+        sales_usa = sales_usa.dropna(how="all") # eliminar files que missing a totes les columnes
 
-    # guardem taula
-    sales_usa.write \
-        .format("jdbc") \
-        .option("url", "jdbc:duckdb:trusted_zone.duckdb") \
-        .option("dbtable", "sales_usa") \
-        .option("driver", "org.duckdb.DuckDBDriver") \
-        .mode("overwrite") \
-        .save()
+        # guardem taula
+        sales_usa.write \
+            .format("jdbc") \
+            .option("url", "jdbc:duckdb:trusted_zone.duckdb") \
+            .option("dbtable", "cleaned_sales") \
+            .option("driver", "org.duckdb.DuckDBDriver") \
+            .mode("overwrite") \
+            .save()
 
-    sales_usa.show()
+        sales_usa.show()
+        print("Trusted zone de SALES, actualitzat")
 
     ###########
     ## SHOPS ##
     ###########
-    #carregar taula de duckdb
-    shops = spark.read \
-        .format("jdbc") \
-        .option("url", "jdbc:duckdb:formatted_zone.duckdb") \
-        .option("dbtable", "shops_data") \
-        .option("driver", "org.duckdb.DuckDBDriver") \
-        .load()
+    if datasearch == True:
+        #carregar taula de duckdb
+        shops = spark.read \
+            .format("jdbc") \
+            .option("url", "jdbc:duckdb:formatted_zone.duckdb") \
+            .option("dbtable", "shops_data") \
+            .option("driver", "org.duckdb.DuckDBDriver") \
+            .load()
 
-    # canviem nom columnes perque no ens deixa accedir-hi si tenen caracters especials
-    # geometry.x
-    new_column_name = "geometry_x"
-    old_column_name = "geometry.x"
-    shops = shops.withColumnRenamed(old_column_name, new_column_name)
+        # canviem nom columnes perque no ens deixa accedir-hi si tenen caracters especials
+        # geometry.x
+        new_column_name = "geometry_x"
+        old_column_name = "geometry.x"
+        shops = shops.withColumnRenamed(old_column_name, new_column_name)
 
-    # geometry.y
-    new_column_name = "geometry_y"
-    old_column_name = "geometry.y"
-    shops = shops.withColumnRenamed(old_column_name, new_column_name)
+        # geometry.y
+        new_column_name = "geometry_y"
+        old_column_name = "geometry.y"
+        shops = shops.withColumnRenamed(old_column_name, new_column_name)
 
-    # attributes.shop
-    new_column_name = "shop"
-    old_column_name = "attributes.shop"
-    shops = shops.withColumnRenamed(old_column_name, new_column_name)
+        # attributes.shop
+        new_column_name = "shop"
+        old_column_name = "attributes.shop"
+        shops = shops.withColumnRenamed(old_column_name, new_column_name)
 
-    # attributes.name
-    new_column_name = "name"
-    old_column_name = "attributes.name"
-    shops = shops.withColumnRenamed(old_column_name, new_column_name)
+        # attributes.name
+        new_column_name = "name"
+        old_column_name = "attributes.name"
+        shops = shops.withColumnRenamed(old_column_name, new_column_name)
 
-    # attributes.osm_id2
-    new_column_name = "index"
-    old_column_name = "attributes.osm_id2"
-    shops = shops.withColumnRenamed(old_column_name, new_column_name)
+        # attributes.osm_id2
+        new_column_name = "index"
+        old_column_name = "attributes.osm_id2"
+        shops = shops.withColumnRenamed(old_column_name, new_column_name)
 
-    # attributes.addr_postcode
-    new_column_name = "postcode"
-    old_column_name = "attributes.addr_postcode"
-    shops = shops.withColumnRenamed(old_column_name, new_column_name)
+        # attributes.addr_postcode
+        new_column_name = "postcode"
+        old_column_name = "attributes.addr_postcode"
+        shops = shops.withColumnRenamed(old_column_name, new_column_name)
 
-    # ens quedem només amb columnes seleccionades
-    selected_columns = ['geometry_x', 'geometry_y', "shop", "name", "index", "postcode"]
-    shops_selected = shops.select(selected_columns)
+        # ens quedem només amb columnes seleccionades
+        selected_columns = ['geometry_x', 'geometry_y', "shop", "name", "index", "postcode"]
+        shops_selected = shops.select(selected_columns)
 
-    # eliminar files que tinguin missings --> excepte les que tenen missings a postcode!!
-    shops_selected = shops_selected.filter(~(col("geometry_x").isNull() |
-                                    col("geometry_y").isNull() |
-                                    col("shop").isNull() |
-                                    col("name").isNull() |
-                                    col("index").isNull()))
+        # eliminar files que tinguin missings --> excepte les que tenen missings a postcode!!
+        shops_selected = shops_selected.filter(~(col("geometry_x").isNull() |
+                                        col("geometry_y").isNull() |
+                                        col("shop").isNull() |
+                                        col("name").isNull() |
+                                        col("index").isNull()))
 
-    # guardem taula
-    shops_selected.write \
-        .format("jdbc") \
-        .option("url", "jdbc:duckdb:trusted_zone.duckdb") \
-        .option("dbtable", "shops_selected") \
-        .option("driver", "org.duckdb.DuckDBDriver") \
-        .mode("overwrite") \
-        .save()
-    
-    shops_selected.show()
+        # guardem taula
+        shops_selected.write \
+            .format("jdbc") \
+            .option("url", "jdbc:duckdb:trusted_zone.duckdb") \
+            .option("dbtable", "cleaned_shops") \
+            .option("driver", "org.duckdb.DuckDBDriver") \
+            .mode("overwrite") \
+            .save()
+        
+        shops_selected.show()
+        print("Trusted zone de SHOPS, actualitzat")
     spark.stop()
     
     
 
-DataQuality()
+DataQuality(income=True, datasearch=True, sales=True, grafiques=True)
